7d5c89c24fabe6b0089792b500a3836f213b4ad5
lebatan815 <tcmedia.copyright@gmail.com>
2026-03-15 03:41:27 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import telebot
 import time
 from deep_translator import GoogleTranslator
 
-TOKEN = "TOKEN_BOT"
-CHAT_ID = "CHAT_ID"
+TOKEN = "8681834599:AAFnYKxIwmgTIJWXYPtPuIsp1ibF0pqfndI"
+CHAT_ID = "1208342332"
 
 UID_LIST = [
 "3493096635463293",
